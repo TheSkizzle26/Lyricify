32f8e766befd5218dbfe651be802c40de6d7eff2
TheSkizzle26 <timobarth12@outlook.de>
2026-03-20 19:44:42 +0100

    switched lyric colors

--- lyrics.py
+++ lyrics.py
@@ -29,8 +29,10 @@ class Lyrics:
         self.num_shown_lines = 5
         self.scroll = InterpolatedValue(0, 0.5)
 
-        self.before_color = (160, 100, 80)
-        self.after_color = (255, 255, 255)
+        # self.before_color = (160, 100, 80)
+        # self.after_color = (255, 255, 255)
+        self.before_color = (255, 255, 255)
+        self.after_color = (160, 100, 80)
 
     def reset(self, song_pos: float):
         self.start_time = pr.get_time() - song_pos
